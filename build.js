53b2973d13db05207bce6b6734c4ae5645e439d7
lizhi123le <34964850+lizhi123le@users.noreply.github.com>
2026-01-20 15:38:29 +0800

    Add files via upload

--- build.js
+++ build.js
@@ -4,7 +4,7 @@ const JavaScriptObfuscator = require('javascript-obfuscator');
 
 // 输入和输出文件路径
 const inputFile = path.join(__dirname, './明文源吗_temp.js');
-const outputFile = path.join(__dirname, './少年你相信光吗.js');
+const outputFile = path.join(__dirname, './少年你相信光吗');
 
 // 确保输出目录存在
 const outputDir = path.dirname(outputFile);
